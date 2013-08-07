7368325eed0ec668759b88633b716e2adbb2c637
atish <atish.lewate@kunalinfotech.net>
2013-08-07 17:35:29 +0530

    completed sign out

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -23,4 +23,12 @@ describe SessionsController do
       end
     end
   end
+
+   describe "DELETE 'destroy'" do
+    it "should sign a user out" do
+      delete :destroy
+      controller.should_not be_signed_in
+      response.should redirect_to(root_path)
+    end
+  end
 end
